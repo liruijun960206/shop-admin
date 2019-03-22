9463e974ed54e44482f85a4a5ca96dca7bf45464
liruijun <1140961146@qq.com>
2019-03-22 16:29:44 +0800

    第一次更新

--- src/App.vue
+++ src/App.vue
@@ -1,31 +1,25 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
     <router-view/>
+    <Home></Home>
   </div>
 </template>
+<script>
+import Home from './components/home/home.vue'
+export default {
+  data () {
+    return {}
+  },
+  methods: {
 
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+  },
+  components: {
+    Home
+  }
 }
-#nav {
-  padding: 30px;
-}
-
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
-}
+</script>
+<style scoped>
+  #app{
+    height: 100%;
+  }
 </style>
